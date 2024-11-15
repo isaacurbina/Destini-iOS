9d5c39c9eb15dc97e22dd0abe6c557b4bcdb0ed7
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2024-11-15 16:16:16 -0600

    116. Steo 7: Show off your work

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -9,16 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var storyLabel: UILabel!
-    @IBOutlet weak var choice1Button: UIButton!
-    @IBOutlet weak var choice2Button: UIButton!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-    }
-
-
+	
+	var storyBrain = StoryBrain()
+	
+	@IBOutlet weak var storyLabel: UILabel!
+	@IBOutlet weak var choice1Button: UIButton!
+	@IBOutlet weak var choice2Button: UIButton!
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		updateUI()
+	}
+	
+	@IBAction func choiceMade(_ sender: UIButton) {
+		let selectedChoice = sender.currentTitle!
+		storyBrain.userChoice(selectedChoice)
+		updateUI()
+	}
+	
+	private func updateUI() {
+		let story = storyBrain.getStory()
+		storyLabel.text = story.title
+		choice1Button.setTitle(story.choice1, for: .normal)
+		choice2Button.setTitle(story.choice2, for: .normal)
+	}
 }
 
